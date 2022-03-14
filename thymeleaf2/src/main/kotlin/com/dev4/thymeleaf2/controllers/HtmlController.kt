1a6199659c816488f4099b1958142dd0001c9d7c
jonideborger <56124097+jonideborger@users.noreply.github.com>
2022-03-14 09:48:35 +0100

    Initial commit

---
+++ thymeleaf2/src/main/kotlin/com/dev4/thymeleaf2/controllers/HtmlController.kt
@@ -0,0 +1,10 @@
+package com.dev4.thymeleaf2.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+}
